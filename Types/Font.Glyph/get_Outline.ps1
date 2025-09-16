6f4e057c7551620f0918ccbbc040a966a4bea99e
StartAutomating <james.brundage@start-automating.com>
2025-09-15 20:45:22 -0700

    feat: `Font.Glyph.get_Outline` ( Fixes #20 )

---
+++ Types/Font.Glyph/get_Outline.ps1
@@ -0,0 +1,16 @@
+$fontFace = $this.ParentNode.'font-face'
+$descent = $fontFace.descent -as [double]
+$bbox = $fontFace.bbox -split '\s' -as [double[]]
+$viewbox = "0 $($bbox[-3] - $descent) $($bbox[-2]) $($bbox[-1])"
+@("<svg xmlns='http://www.w3.org/2000/svg' viewBox='$viewbox' width='100%' height='100%' transform='scale(1 -1)'>"
+
+"<path stroke='currentColor' d='$($this.PathData)' />"
+@"
+<circle r="5" fill="red">
+    <animateMotion
+      dur="10s"
+      repeatCount="indefinite"
+      path="$($pathData)" />
+  </circle>
+"@
+"</svg>") -as [xml]
