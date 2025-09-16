96779076a9a49be1a208f639ef3197c86cfc9aed
StartAutomating <james.brundage@start-automating.com>
2025-09-15 20:01:05 -0700

    feat: `Font.svg.GetGlyph()` ( Fixes #15 )
    Looking for unicode or name, and defaulting to finding everything if no value is passed

--- Types/Font.svg/GetGlyph.ps1
+++ Types/Font.svg/GetGlyph.ps1
@@ -2,8 +2,19 @@ param(
 [PSObject]$value
 )
 
+filter toGlyph {    
+    $_.Node.pstypenames.add('Font.glyph')
+    $_.Node
+}
 if ($value -is [string] -and $value.Length -le 2) {
-    $this.SVG | 
-        Select-Xml -Namespace @{s='http://www.w3.org/2000/svg'} -XPath "//s:glyph[@unicode='$($value -replace "'", "''")']" |
-        Select-Object -ExpandProperty Node
+    $escapedValue = $($value -replace "'","''")
+    $this.XML | 
+        Select-Xml -Namespace @{s='http://www.w3.org/2000/svg'} -XPath "//s:glyph[@unicode='$escapedValue'] | //s:glyph[@glyph-name='$escapedValue']" | 
+        toGlyph
+}
+
+if ($null -eq $value) {
+    $this.XML | 
+        Select-Xml -Namespace @{s='http://www.w3.org/2000/svg'} -XPath "//s:glyph" |
+        toGlyph
 }
