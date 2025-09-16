c6238f86074c57cb2cb33a162f515cceb0f41dbd
StartAutomating <james.brundage@start-automating.com>
2025-09-16 12:01:04 -0700

    fix: `Font.Glyph.get_SVG` ( Fixes #18 )
    Using bounding box

--- Types/Font.Glyph/get_SVG.ps1
+++ Types/Font.Glyph/get_SVG.ps1
@@ -1,9 +1,9 @@
 $fontFace = $this.ParentNode.'font-face'
 $descent = $fontFace.descent -as [double]
 $bbox = $fontFace.bbox -split '\s' -as [double[]]
-$viewbox = "0 $($bbox[-3] - $descent) $($bbox[-2]) $($bbox[-1])"
+$viewbox = $bbox
 @(
     "<svg xmlns='http://www.w3.org/2000/svg' viewBox='$viewbox' width='100%' height='100%' transform='scale(1 -1)'>"
-    "<path fill='currentColor' d='$($this.PathData)' />"    
+    "<path fill='currentColor' d='$($this.PathData)'  />"
     "</svg>"
 ) -as [xml]
