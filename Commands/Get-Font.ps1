9d534090da02c2ed46808198552f1eb09d8a98a7
StartAutomating <james.brundage@start-automating.com>
2025-09-16 13:01:27 -0700

    docs: `Get-Font` Help ( Fixes #2 )

--- Commands/Get-Font.ps1
+++ Commands/Get-Font.ps1
@@ -4,16 +4,19 @@ function Get-Font {
         Gets Fonts
     .DESCRIPTION
         Gets currently installed fonts
+    .EXAMPLE
+        Get-Font
     #>
     param()
 
+    # Fonts will be in different places, depending on the operating system
     $fontPaths = 
         if ($IsLinux) {
             "/usr/share/fonts"
-            "$home/.local/share/fonts"
+            "~/.local/share/fonts"
         } elseif ($IsMacOS) {
             "/Library/Fonts"
-            "$home/Library/Fonts"
+            "~/Library/Fonts"
         } else {
             "$env:WinDir\Fonts"
             "$($env:AppData | 
@@ -21,8 +24,10 @@ function Get-Font {
                 Join-Path -ChildPath 'Local\Microsoft\Windows\Fonts')"
         }
 
+    # If we have fc-list, it will be quicker and more authoritative 
     $fcList = $ExecutionContext.SessionState.invokecommand.GetCommand('fc-list', 'Application')
 
+    # Collect all of the font files
     $fontFiles =
         if ($fcList) {        
             & $fcList | 
@@ -38,13 +43,16 @@ function Get-Font {
         }
 
     
+    # Walk thru each list of collected files
     foreach ($fontFile in $fontFiles) {
-        if ($fontFile.Extension -notin '.svg','.ttf','.otf') {
+        # and as long as they appear to be a font
+        if ($fontFile.Extension -notin '.svg', '.ttf', '.otf', '.t1', '.pfb') {
             continue
         }
+        # decorate them as a `Font.File`
         $fontFile.pstypenames.add('Font.File')
+        # and output the font.
         $fontFile
-    }
-    
+    }    
 }
 
