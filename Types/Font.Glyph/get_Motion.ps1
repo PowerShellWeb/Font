25cb7d3c211e9ae995f70f1e816ce2a1e3e52e83
StartAutomating <james.brundage@start-automating.com>
2025-09-15 20:47:51 -0700

    feat: `Font.Glyph.get_Motion` ( Fixes #21 )

---
+++ Types/Font.Glyph/get_Motion.ps1
@@ -0,0 +1,19 @@
+$fontFace = $this.ParentNode.'font-face'
+$descent = $fontFace.descent -as [double]
+$bbox = $fontFace.bbox -split '\s' -as [double[]]
+$viewbox = "0 $($bbox[-3] - $descent) $($bbox[-2]) $($bbox[-1])"
+$PathData = $this.PathData
+@(
+    "<svg xmlns='http://www.w3.org/2000/svg' viewBox='$viewbox' width='100%' height='100%' transform='scale(1 -1)'>"
+    "<path stroke='currentColor' d='$($PathData)'  />"
+@"
+<circle r="5" fill="currentColor">
+    <animateMotion
+      dur="10s"
+      repeatCount="indefinite"
+      path="$($PathData)" />
+  </circle>
+"@
+    "</svg>"
+) -as [xml]
+
