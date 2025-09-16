951493df4b04f3ccd87e05375567814f7e2a7d5c
StartAutomating <james.brundage@start-automating.com>
2025-09-16 12:05:36 -0700

    fix: `Font.Glyph.get_Motion/Outline` ( Fixes #20, Fixes #21 )
    Using bounding box

--- Types/Font.Glyph/get_Motion.ps1
+++ Types/Font.Glyph/get_Motion.ps1
@@ -1,7 +1,6 @@
 $fontFace = $this.ParentNode.'font-face'
 $descent = $fontFace.descent -as [double]
-$bbox = $fontFace.bbox -split '\s' -as [double[]]
-$viewbox = "0 $($bbox[-3] - $descent) $($bbox[-2]) $($bbox[-1])"
+$viewbox = $fontFace.bbox -split '\s' -as [double[]]
 $PathData = $this.PathData
 @(
     "<svg xmlns='http://www.w3.org/2000/svg' viewBox='$viewbox' width='100%' height='100%' transform='scale(1 -1)'>"
