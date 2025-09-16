ceb7bd3901f7c0dc7328a2fd802a7750ce13095c
StartAutomating <james.brundage@start-automating.com>
2025-09-16 13:11:34 -0700

    test: Adjusting Font Tests ( Fixes #16 )

--- Font.tests.ps1
+++ Font.tests.ps1
@@ -1,11 +1,12 @@
 describe Font {
     it 'Gets fonts' {
-        Get-Font
+        $installedFonts = Get-Font 
+        $installedFonts.Count | Should -BeGreaterThan 1        
     }
 
     it 'Can import a font' {
         $importedFont = Get-Font | Get-Random | Import-Font
         $importedFont.FontFamily | Should -BeLike '*'
         $importedFont.UnitsPerEm | Should -BeGreaterThan 0
-    }
+    }    
 }
